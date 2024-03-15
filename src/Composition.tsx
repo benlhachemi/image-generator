556a8ca1181d31f3357968e3bc8922cf93fcfb5b
Souhail Benlhachemi <souhailbenlhachemi1999@gmail.com>
2024-03-15 21:39:08 +0000

    aaaaaa

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,28 +1,28 @@
-import {AbsoluteFill} from 'remotion';
-import {Logo} from './Logo';
-import {Subtitle} from './Subtitle';
-import {Title} from './Title';
+// Imports
+import {staticFile} from 'remotion'
 import {z} from 'zod';
-import {zColor} from '@remotion/zod-types';
 
 export const myCompSchema = z.object({
-	titleText: z.string(),
-	titleColor: zColor(),
-	logoColor: zColor(),
+	text: z.string(),
 });
 
-export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
-	titleText: propOne,
-	titleColor: propTwo,
-	logoColor: propThree,
-}) => {
+export const MyComposition = ({text, bgImage, isBorder, border}: any) => {
 	return (
-		<AbsoluteFill className="bg-gray-100 items-center justify-center">
-			<div className="m-10" />
-			<Logo logoColor={propThree} />
-			<div className="m-3" />
-			<Title titleText={propOne} titleColor={propTwo} />
-			<Subtitle />
-		</AbsoluteFill>
+		<>
+			<div 
+				className='w-full h-full bg-red-500 font-black text-7xl flex items-center justify-center bg-cover bg-center backdrop-blur-lg' 
+				style={{background: `url(${staticFile(bgImage)})`}}
+			>
+				<h1 className="text-center px-14 leading-normal uppercase bg-black bg-opacity-30 text-yellow-400 drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]">{text}</h1>
+			</div>
+
+			{/* border */}
+			{isBorder ? (
+				<img className='absolute w-full h-full z-50' src={staticFile(border)} />
+			) : ''}
+
+			{/* WATERMARK */}
+			<div className="absolute bottom-7 right-7 z-50 text-3xl font-medium text-white bg-black bg-opacity-50 py-4 px-9 -rotate-12 rounded-2xl shadow-lg">OMAR SLAOUI</div>
+		</>
 	);
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,22 +1,31 @@
-import {Composition} from 'remotion';
+import {Still, random} from 'remotion';
 import {MyComposition, myCompSchema} from './Composition';
 import './style.css';
+
+function getRandomBg(): string {
+  const randomIndex = Math.floor(random(null) * 39) + 1;
+  return `/bg/${randomIndex}.jpg`;
+}
+
+function getRandomBorder(): string {
+  const randomIndex = Math.floor(random(null) * 9) + 1;
+  return `/borders/${randomIndex}.png`;
+}
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
-			<Composition
+			<Still
 				id="MyComp"
 				component={MyComposition}
-				durationInFrames={240}
-				fps={30}
 				width={1280}
 				height={720}
 				schema={myCompSchema}
 				defaultProps={{
-					titleText: 'Welcome to Remotion with Tailwind CSS',
-					titleColor: '#000000',
-					logoColor: '#00bfff',
+					text: 'hello this is just a test sample text for image generation thank you so much',
+					bgImage: getRandomBg(),
+					border: getRandomBorder(),
+					isBorder: Math.floor(random(null) * 2),
 				}}
 			/>
 		</>
